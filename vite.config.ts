9a1ae97574c304c7c1e1211e0edb931a1e93ade6
harshjohiya <harsh24@iiserb.ac.in>
2026-02-09 18:00:57 +0530

    make some change

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
@@ -16,7 +15,6 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     tsconfigPaths(),
-    mode === "development" && componentTagger()
   ].filter(Boolean),
   resolve: {
     alias: {
